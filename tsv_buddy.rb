e84bbc5836e0aa60eeb05aa2828ea9d760ae234d
emily <emiliyvicki601@gmail.com>
2021-10-10 19:41:24 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,26 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
-  def take_tsv(tsv)
 
+  NEWLINE = "\n"
+  TAB = "\t"
+
+  def take_tsv(tsv)
+    @data = []
+    rows = tsv.split(NEWLINE).map { |line| line.split(TAB)}
+    headers = rows.first
+    data_rows = rows[1..]
+    @data = data_rows.map { |row| headers.zip(row).to_h }
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    headers = @data[0].each_key.map { |key| key }
+    rows = @data.map { |row| NEWLINE + row.values.join(TAB)}
+    [headers, rows, NEWLINE].join
   end
 end
